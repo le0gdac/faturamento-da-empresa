510d47671c4b7dfb7392b2f2d1d278777086b4a0
unknown <leonardo.gabriel98@hotmail.com>
2023-03-17 09:19:17 -0300

    programa finalizado

--- analise-faturamento.py
+++ analise-faturamento.py
@@ -1,8 +1,24 @@
 import json
 
-with open("dados.json") as dados_faturamento:
-    dados = json.load(dados_faturamento)
+with open('dados.json', 'r') as arquivo:
+    dados = json.load(arquivo)
 
-menor_valor = (min(dados_faturamento))
-print(menor_valor)
+total_faturamento = 0
+dias_com_faturamento = 0
+menor_faturamento = float('inf')
+maior_faturamento = float('-inf')
 
+for item in dados:
+    if item['valor'] > 0:
+        total_faturamento += item['valor']
+        dias_com_faturamento += 1
+        if item['valor'] < menor_faturamento:
+            menor_faturamento = item['valor']
+        if item['valor'] > maior_faturamento:
+            maior_faturamento = item['valor']
+
+media_mensal = total_faturamento / dias_com_faturamento
+
+print("Menor valor de faturamento: R$ {:.2f}".format(menor_faturamento))
+print("Maior valor de faturamento: R$ {:.2f}".format(maior_faturamento))
+print("Número de dias em que o faturamento foi superior à média mensal: {}".format(sum(item['valor'] > media_mensal for item in dados if item['valor'] > 0)))
